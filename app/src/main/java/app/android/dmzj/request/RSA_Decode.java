14369a5a9cf42cb3196dc6bd83ec8d64aebfd4b3
dongby <kitakami_hibiki@outlook.com>
2024-02-10 21:17:31 +0800

    解决解密错误bug

--- app/src/main/java/app/android/dmzj/request/RSA_Decode.java
+++ app/src/main/java/app/android/dmzj/request/RSA_Decode.java
@@ -4,41 +4,36 @@ import javax.crypto.Cipher;
 
 import java.io.ByteArrayOutputStream;
 import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
 public class RSA_Decode {
     static String V4_PRIVATE_KEY = "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAK8nNR1lTnIfIes6oRWJNj3mB6OssDGx0uGMpgpbVCpf6+VwnuI2stmhZNoQcM417Iz7WqlPzbUmu9R4dEKmLGEEqOhOdVaeh9Xk2IPPjqIu5TbkLZRxkY3dJM1htbz57d/roesJLkZXqssfG5EJauNc+RcABTfLb4IiFjSMlTsnAgMBAAECgYEAiz/pi2hKOJKlvcTL4jpHJGjn8+lL3wZX+LeAHkXDoTjHa47g0knYYQteCbv+YwMeAGupBWiLy5RyyhXFoGNKbbnvftMYK56hH+iqxjtDLnjSDKWnhcB7089sNKaEM9Ilil6uxWMrMMBH9v2PLdYsqMBHqPutKu/SigeGPeiB7VECQQDizVlNv67go99QAIv2n/ga4e0wLizVuaNBXE88AdOnaZ0LOTeniVEqvPtgUk63zbjl0P/pzQzyjitwe6HoCAIpAkEAxbOtnCm1uKEp5HsNaXEJTwE7WQf7PrLD4+BpGtNKkgja6f6F4ld4QZ2TQ6qvsCizSGJrjOpNdjVGJ7bgYMcczwJBALvJWPLmDi7ToFfGTB0EsNHZVKE66kZ/8Stx+ezueke4S556XplqOflQBjbnj2PigwBN/0afT+QZUOBOjWzoDJkCQClzo+oDQMvGVs9GEajS/32mJ3hiWQZrWvEzgzYRqSf3XVcEe7PaXSd8z3y3lACeeACsShqQoc8wGlaHXIJOHTcCQQCZw5127ZGs8ZDTSrogrH73Kw/HvX55wGAeirKYcv28eauveCG7iyFR0PFB/P/EDZnyb+ifvyEFlucPUI0+Y87F";
 
-    public byte[] decryptByPrivateKey(String content) {
-        try {
-            Cipher cipher = Cipher.getInstance("RSA");
-            PrivateKey priKey;
-            priKey = KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder().decode(V4_PRIVATE_KEY)));
-            cipher.init(Cipher.DECRYPT_MODE, priKey);
-            byte[] encryptedData = Base64.getDecoder().decode(content);
-            int inputLen = encryptedData.length;
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            int offSet = 0;
-            byte[] cache;
-            int i = 0;
-            // 对数据分段解密
-            while (inputLen - offSet > 0) {
-                if (inputLen - offSet > 128) {
-                    cache = cipher.doFinal(encryptedData, offSet, 128);
-                } else {
-                    cache = cipher.doFinal(encryptedData, offSet, inputLen - offSet);
-                }
-                out.write(cache, 0, cache.length);
-                i++;
-                offSet = i * 128;
-            }
-            byte[] decryptedData = out.toByteArray();
-            out.close();
-            return decryptedData;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    public byte[] decryptByPrivateKey(String content) throws Exception {
+        byte[] inputByte = Base64.getDecoder().decode(content);
+        //base64编码的私钥
+        byte[] decoded = Base64.getDecoder().decode(V4_PRIVATE_KEY);
+        RSAPrivateKey priKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(decoded));
+        //RSA解密
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, priKey);
+        int count = 0;
+        int start = 0;
+        int end = 0;
+        byte[] out = new byte[0];
+        while (start<inputByte.length){
+            end = start+128;
+            if(end>inputByte.length)
+                end=inputByte.length;
+            out = NetConnection.byteCat(out,cipher.doFinal(NetConnection.subBytes(inputByte,start,end)));
+            count+=1;
+            start = 128*count;
         }
+        return out;
     }
 }
