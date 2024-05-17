49686ee5fc2d2cac9a3bebe203cc7c68fc186f8b
KitakamiHibiki <92364193+KitakamiHibiki@users.noreply.github.com>
2024-05-17 15:26:58 +0800

    Update Request_User.kt

--- app/src/main/java/app/android/dmzj/request/user/Request_User.kt
+++ app/src/main/java/app/android/dmzj/request/user/Request_User.kt
@@ -10,7 +10,6 @@ import java.lang.Exception
 //登录并获取个人消息
 fun login(nickname: String, pwd: String): String {
 //    nickname:kitakamihibiki
-//    pwd:359vbhg+
     val url = api.BASE_URL_USER + "/loginV2/m_confirm"
     val HM = HashMap<String, String>()
     HM["nickname"] = nickname
